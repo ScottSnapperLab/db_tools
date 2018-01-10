8a15d7685e32f086222ccd6a3d67f6cccf681ff9
Gus Dunn <w.gus.dunn@gmail.com>
2018-01-10 11:30:06 -0500

    Added `etl` subpackage

---
+++ db_tools/etl/redcap/loaders.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Provide code to organize the validation and recoding of redcap db dumps."""
+
+# Imports
+from 
+
+# Constants
+
+# Functions
+
+if __name__ == "__main__":
+    pass
