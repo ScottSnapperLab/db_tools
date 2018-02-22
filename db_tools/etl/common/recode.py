fadcbb5fdc141f6d51bc7d1b0c0834bc31d00680
Gus Dunn <w.gus.dunn@gmail.com>
2018-02-22 16:45:45 -0500

    etl.common.recode: added nan_to_none, to_str, to_one_or_zero, setify_drop_nones

---
+++ db_tools/etl/common/recode.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""Provide code to recode columns common to many sources."""
+
+# Imports
+import pandas as pd
+
+
+def nan_to_none(series):
+    """Return series after converting NaN-type objects to ``None``."""
+    return series.where(pd.notna(series), None)
+
+
+def to_str(series):
+    """Return series with data re-cast as strings."""
+    return series.astype(str)
+
+
+def to_one_or_zero(series):
+    """Recode series values to either [0,1,None]."""
+    valid_vals = set([0, 1])
+    series = series.fillna(0).astype(int)
+
+    obs_vals = set(series.unique())
+
+    if not obs_vals.issubset(valid_vals):
+        invalid_vals = obs_vals - valid_vals
+        raise ValueError(f"Encountered non-valid values: {invalid_vals}.")
+
+    return series
+
+
+def setify_drop_nones(series):
+    """Convert to sets and drop ``None`` values."""
+
+    def drop_nones(x):
+        x.discard(None)
+        return x
+
+    return series.apply(lambda x: set(x)).apply(drop_nones).apply(list)
