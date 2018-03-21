d32af1608d4d30f2c843607387734d98b865f3bf
Gus Dunn <w.gus.dunn@gmail.com>
2018-03-21 11:08:32 -0400

    etl.redcap: class RedCapColumnInfo

--- db_tools/etl/redcap/__init__.py
+++ db_tools/etl/redcap/__init__.py
@@ -195,4 +195,84 @@ def yesno_column_factory(ddict_col):
         unique=False,
         validators=[valid_values],
         recoders=[translate_column],
+#TODO: Add custom range validators to column_object if the data is provided in the data_dict
+@dataclass(init=False)
+class RedCapColumnInfo(object):
+    """FILL THIS IN."""
+
+    name: str = None
+    form_name: str = None
+    section_header: str = None
+    field_type: str = None
+    field_label: str = None
+    options_labels: dict = None
+    validation_kind: str = None
+    validation_min: typ.Any = None
+    validation_max: typ.Any = None
+    column_object: BaseColumn = None
+
+    def __init__(self, ddict_col):
+        """Initiate the TextTypeInfo object."""
+        self.name = ddict_col.name
+        self.form_name = ddict_col["Form Name"]
+        self.section_header = ddict_col["Section Header"]
+        self.field_type = ddict_col["Field Type"]
+        self.field_label = ddict_col["Field Label"]
+
+        self.options_labels = ccs_labels_to_mapper(ddict_col)
+
+        self.validation_kind = ddict_col["Text Validation Type OR Show Slider Number"]
+        self.validation_min = ddict_col["Text Validation Min"]
+        self.validation_max = ddict_col["Text Validation Max"]
+
+        self._cast_validation_limits()
+        self._spawn_column_object()
+
+    def _cast_validation_limits(self):
+        """Recast self.validation_min/validation_max based on cast_map."""
+
+        def build_robust_casting_func(func):
+            """Return function to recast using ``func`` after adding error-catching logic."""
+
+            def robust_casting_func(value):
+                """Recast ``value`` with ``func`` unless ``func`` fails: then use fall-back."""
+                try:
+                    return func(value)
+                except (ValueError, TypeError):
+                    log.warn(
+                        f"`{value}` failed to be recast with function `{func.__name__}`, Falling back to `str(value)`."
+                    )
+                    return str(value)
+
+            return robust_casting_func
+
+        cast_map = {
+            "time": build_robust_casting_func(lambda txt: pen.parse(txt).time()),
+            "alpha_only": build_robust_casting_func(str),
+            "date_ymd": build_robust_casting_func(lambda txt: pen.parse(txt).date()),
+            "date_mdy": build_robust_casting_func(lambda txt: pen.parse(txt).date()),
+            "date_dmy": build_robust_casting_func(lambda txt: pen.parse(txt).date()),
+            "integer": build_robust_casting_func(int),
+            "number": build_robust_casting_func(float),
+            "number_1dp": build_robust_casting_func(float),
+            "number_4dp": build_robust_casting_func(float),
+            np.nan: build_robust_casting_func(str),
+        }
+
+        recast = cast_map[self.validation_kind]
+        self.validation_min = recast(self.validation_min)
+        self.validation_max = recast(self.validation_max)
+
+    def _spawn_column_object(self):
+        """Create and store the Column Object."""
+        fieldtype_to_factory = {
+            "radio": radio_dropdown_column_factory,
+            "text": text_column_factory,
+            "checkbox": checkbox_column_factory,
+            "dropdown": radio_dropdown_column_factory,
+            "yesno": yesno_column_factory,
+            "calc": calc_column_factory,
+        }
+
+        self.column_object = fieldtype_to_factory[self.field_type](self)
     )
