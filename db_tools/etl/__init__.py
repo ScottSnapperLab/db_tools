6c5910d4c2be4dc60f51e784da1613c38e41afa2
Gus Dunn <w.gus.dunn@gmail.com>
2018-03-21 13:14:37 -0400

    moved time_string_to_time to etl.__init__

--- db_tools/etl/__init__.py
+++ db_tools/etl/__init__.py
@@ -1,8 +1,13 @@
 """Provide top level code in support of extract-transform-load type operations."""
+import pandas as pd
+
+import pendulum as pen
+
 
 def get_failed_values(df, col):
     failed = col.validate(col.recode(df), failed_only=True)
     return df[col.name].iloc[failed.index]
+
 
 def is_subset(x, ref_set):
     """Return ``True`` if ``x`` is a subset of ``ref_set``."""
@@ -15,3 +20,15 @@ def is_subset(x, ref_set):
         set_x = set([x])
 
     return set_x.issubset(ref_set)
+
+
+def time_string_to_time(time_string):
+    """Return datetime.time instance from a time string."""
+    try:
+        return pen.parse(time_string).time()
+    except TypeError:
+        if pd.isnull(time_string):
+            return pd.NaT
+
+
+
