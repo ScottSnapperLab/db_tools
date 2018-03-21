e6f095ba6ff0ac0bd4beaccaa79d8ee90d54a59d
Gus Dunn <w.gus.dunn@gmail.com>
2018-03-21 10:58:23 -0400

    etl.common.validate: isalpha, istime

--- db_tools/etl/common/validate.py
+++ db_tools/etl/common/validate.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python
 """Provide code to validate columns common to many sources."""
+
+import datetime as dt
+import pandas as pd
+
+
+def isalpha(series):
+    """Enforce that values contain only letters."""
+    return series.astype(str).str.isalpha()
+
+
+def istime(series):
+    """Enforce that the values are time-like"""
+
+    def test(value):
+        valid_types = (
+            dt.time,
+            type(pd.NaT),
+        )
+        return isinstance(value, valid_types)
+
+    return series.apply(test)
