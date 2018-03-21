f57d20072123f82eebaa8e4193760d0d0d409268
Gus Dunn <w.gus.dunn@gmail.com>
2018-03-21 11:00:22 -0400

    etl.redcap.loaders: load_data_dict

--- db_tools/etl/redcap/loaders.py
+++ db_tools/etl/redcap/loaders.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 """Provide code to organize the validation and recoding of redcap db dumps."""
+import pandas as pd
+
+
+def load_data_dict(path):
+    """Load a redcap data_dict file and transpose it so that column names represent actual column names."""
+    return pd.read_csv(path, dtype=str).set_index("Variable / Field Name").T
